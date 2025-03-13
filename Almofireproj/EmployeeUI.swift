3dedc577b7cade81cc8b60f9f0efe2df8181ea97
mohammadaboelnasr <m.aboelnasr@live.com>
2025-03-14 02:59:15 +0300

    cats

---
+++ Almofireproj/EmployeeUI.swift
@@ -0,0 +1,18 @@
+//
+//  EmployeeUI.swift
+//  Almofireproj
+//
+//  Created by Mohammad Aboelnasr on 13/03/2025.
+//
+
+import SwiftUI
+
+struct EmployeeUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmployeeUI()
+}
---
+++ Almofireproj/empolyee.swift
@@ -0,0 +1,8 @@
+//
+//  empolyee.swift
+//  Almofireproj
+//
+//  Created by Mohammad Aboelnasr on 13/03/2025.
+//
+
+import Foundation
